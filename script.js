964748f0ae48e645391d343a311401bf47a0ed52
Rua-N <dmailruan@gmail.com>
2024-02-19 08:35:43 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function Ativar() {
+  const html = document.documentElement
+  //essa função confere sozinha se tem ou não a classe light
+  html.classList.toggle("light")
+  // trocando img
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light-qq.png")
+    img.setAttribute("alt", "My beautiful GF, damn I really like her")
+  } else {
+    img.setAttribute("src", "./assets/avatar-ruan.png")
+    img.setAttribute(
+      "alt",
+      "Picture of Ruan Dias smiling in the gym, with his glasses,backwards hat and a blue shirt"
+    )
+  }
+}
